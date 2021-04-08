40aaf715b806a7397b0d098939e5c954fb0584f7
Mariah <diazma@umich.edu>
2021-04-08 18:35:14 -0400

    Added code to test the AddHeader Method

--- commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -45,7 +45,7 @@ public class EmailTest {
   		email.addBcc(Empty_Emails);
   		assertEquals(0, email.getBccAddresses().size());
   	}
-    
+
     //The following code tests addCC
     	@Test
     	public void testAddCc() throws Exception
@@ -60,3 +60,25 @@ public class EmailTest {
     		email.addCc("");
     		assertEquals(0, email.getCcAddresses().size());
     	}
+
+      //The following code tests addHeader!
+	@Test
+	public void testAddHeader() throws Exception
+	{
+		email.addHeader(Test_Names[0] , testValidChars[3]);
+		assertEquals(1, email.getHeaders().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddHeaderValue() throws Exception
+	{
+		email.addHeader("" , testValidChars[3]);
+		assertEquals(0, email.getHeaders().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddHeaderName() throws Exception
+	{
+		email.addHeader(Test_Names[1], "");
+		assertEquals(0, email.getHeaders().size());
+	}
